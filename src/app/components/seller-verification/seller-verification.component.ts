15a40423b8df77caab75dd5662a9a0ee363000bb
Fikunmi01 <fikunmiadekunle@gmail.com>
2025-02-15 03:47:48 +0100

    Update

--- src/app/components/seller-verification/seller-verification.component.ts
+++ src/app/components/seller-verification/seller-verification.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -14,21 +14,14 @@ import {
   templateUrl: './seller-verification.component.html',
   styleUrl: './seller-verification.component.scss',
 })
-export class SellerVerificationComponent implements OnInit {
-  @ViewChild('videoElement') videoElement!: ElementRef;
+export class SellerVerificationComponent {
   steps = ['Personal Information', 'Document Upload', 'Facial Verification'];
   currentStep = 1;
   personalInfoForm: FormGroup;
   selectedFile: File | null = null;
   photoTaken = false;
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.initForm();
-  }
-
-  initForm() {
+  constructor(private fb: FormBuilder) {
     this.personalInfoForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -67,34 +60,7 @@ export class SellerVerificationComponent implements OnInit {
   }
 
   togglePhoto(): void {
-    if (this.photoTaken) {
-      this.photoTaken = false;
-      this.startCamera();
-    } else {
-      this.capturePhoto();
-    }
-  }
-
-  startCamera() {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(stream => {
-        this.videoElement.nativeElement.srcObject = stream;
-        this.videoElement.nativeElement.play();
-      })
-      .catch(error => {
-        console.error('Error accessing camera:', error);
-      });
-  }
-
-  capturePhoto() {
-    const video = this.videoElement.nativeElement;
-    const canvas = document.createElement('canvas');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    this.photoTaken = true;
-    video.srcObject.getTracks().forEach((track: any) => track.stop());
+    this.photoTaken = !this.photoTaken;
   }
 
   completeVerification(): void {
